accbd6cd939fa22e0711f0c63bffd4bba1658089
hartmut <hbrng.computer@mailbox.org>
2022-01-04 20:51:02 +0100

    structure for short-help

--- src/main.lisp
+++ src/main.lisp
@@ -8,49 +8,63 @@
 (in-package :lisped)
 
 (defun create-key-button (name icon)
-  (let* ((button (make-instance 'gtk-button :width-request 90))
-	(box    (make-instance 'gtk-box :orientation :vertical :spacing 4))
-	(label  (make-instance 'gtk-label :use-markup t :label name)))
+  (let ((button (make-instance 'gtk-button :width-request 90))
+	 (box    (make-instance 'gtk-box :orientation :vertical :spacing 4))
+	 (label  (make-instance 'gtk-label :use-markup t :label name)))
     (gtk-box-pack-start box (gtk-image-new-from-file icon))
     (gtk-box-pack-start box label) 
-    (gtk-container-add button box)
-    
+    (gtk-container-add button box)  
     button))
 
-(defparameter default-f-key-configs
-  (list (cons 'ESC "icons/empty.png")
-	(cons 'F1  "icons/help.png")
-	(cons 'F2  "icons/menu.svg")
-	(cons 'F3  "icons/new_file.svg")
-	(cons 'F4  "icons/open_file.svg")
-	(cons 'F5  "icons/save_file.svg")
-	(cons 'F6  "icons/save_file_as.svg")
-	(cons 'F7  "icons/close.svg")
-	(cons 'F8  "icons/undo.svg")
-	(cons 'F9  "icons/redo.svg")
-	(cons 'F10  "icons/copy.svg")
-	(cons 'F11  "icons/paste.svg")
-	(cons 'F12  "icons/search.svg")))
+(defun create-short-help-tooltip (text)
+  (let ((css-provider (make-instance 'gtk-css-provider))
+	(box          (make-instance 'gtk-box))
+	(label        (make-instance 'gtk-label :margin 5 :label text)))
+    (gtk-css-provider-load-from-path css-provider "css.txt")
+    (gtk-container-add box label)
+    (gtk-style-context-add-provider (gtk-widget-get-style-context box)
+				    css-provider
+				    +gtk-style-provider-priority-application+)
+    box)
+  )
+
+(defstruct key-definition icon-file short-help long-help)
+(defstruct key-instance   button action short-help long-help)   
+
+(defparameter *esc-and-f-keys-definitions*
+  (list (cons 'ESC  (make-key-definition :icon-file "icons/empty.png"        :short-help "" :long-help"" ))
+	(cons 'F1   (make-key-definition :icon-file "icons/help.png"         :short-help "help" :long-help"" ))
+	(cons 'F2   (make-key-definition :icon-file "icons/menu.svg"         :short-help "" :long-help"" ))
+	(cons 'F3   (make-key-definition :icon-file "icons/new_file.svg"     :short-help "" :long-help"" ))
+	(cons 'F4   (make-key-definition :icon-file "icons/open_file.svg"    :short-help "" :long-help"" ))
+	(cons 'F5   (make-key-definition :icon-file "icons/save_file.svg"    :short-help "" :long-help"" ))
+	(cons 'F6   (make-key-definition :icon-file "icons/save_file_as.svg" :short-help "" :long-help"" )) 
+ 	(cons 'F7   (make-key-definition :icon-file "icons/close.svg"        :short-help "" :long-help"" ))
+	(cons 'F8   (make-key-definition :icon-file "icons/undo.svg"         :short-help "" :long-help"" ))
+	(cons 'F9   (make-key-definition :icon-file "icons/redo.svg"         :short-help "" :long-help"" ))
+	(cons 'F10  (make-key-definition :icon-file "icons/copy.svg"         :short-help "" :long-help"" ))
+	(cons 'F11  (make-key-definition :icon-file "icons/paste.svg"        :short-help "" :long-help"" ))
+	(cons 'F12  (make-key-definition :icon-file "icons/search.svg"       :short-help "" :long-help"" ))))
 
 (defun create-empty-keys ()
-  (mapcar (lambda (key-conf)
-	    (cons (car key-conf) (create-key-button (string (car key-conf)) (cdr key-conf))))
-	    default-f-key-configs))
+  (mapcar (lambda (key-definition)
+	    (cons (car key-definition)
+		  (make-key-instance :button (create-key-button (string (car key-definition)) (key-definition-icon-file (cdr key-definition)))
+				     :action nil 
+				     :short-help (create-short-help-tooltip (key-definition-short-help (cdr key-definition)))
+				     :long-help nil)))
+	  *esc-and-f-keys-definitions*))
+
+(defparameter *empty-f-keys* nil)
 
 (defun addkeys (box)
   (dolist (key-button *empty-f-keys*)
-    (gtk-box-pack-start box (cdr key-button) :expand nil)))
+    (gtk-box-pack-start box (key-instance-button (cdr key-button)) :expand nil)))
 
-(defparameter *empty-f-keys* nil)
 (defparameter *f-keys-box* nil)
 (defparameter *win* nil)
 (defparameter *help-overlay* nil)
 (defparameter *help-test-widget* nil)
-
-(defparameter +tooltip-css+
-  "GtkLabel {
-    color: green;
-   }")
 
 (defun create-mainwindow ()
   (setf *empty-f-keys* (create-empty-keys))
@@ -64,31 +78,23 @@
           (main-box (make-instance 'gtk-box
                               :orientation :vertical
                               :spacing 6))
-	  (provider (make-instance 'gtk-css-provider))
 	  (textview (make-instance 'gtk-text-view
                                    :wrap-mode :word
                                    :top-margin 2
                                    :left-margin 2
                                    :right-margin 2)))
-      (setf *f-key-box* (make-instance 'gtk-box
+      (setf *f-keys-box* (make-instance 'gtk-box
 				      :orientation :horizontal
 				      :spacing 4))
-      (addkeys *f-key-box*)
-      (gtk-box-pack-start main-box *f-key-box* :expand nil)
+      (addkeys *f-keys-box*)
+      (gtk-box-pack-start main-box *f-keys-box* :expand nil)
       (gtk-box-pack-start main-box textview)
       (setf *help-overlay* (make-instance 'gtk-overlay))
-      (setf *help-test-widget* (make-instance 'gtk-label :label "Hallo"))
       (gtk-container-add *help-overlay* main-box)
       (gtk-container-add window *help-overlay*)
-      (gtk-css-provider-load-from-path provider "css.txt")
       (let ((fixed (make-instance 'gtk-fixed))
-	    (box   (make-instance 'gtk-box))
-	    (label (make-instance 'gtk-label :margin 5 :label "Hallo Das ist der Tooltip")))
-	(gtk-container-add box label)
-	(gtk-style-context-add-provider (gtk-widget-get-style-context box)
-					provider
-					+gtk-style-provider-priority-application+)
-	(gtk-fixed-put fixed box 20 20)
+	    (tooltip (create-short-help-tooltip "Ein Beispiel lsjdf")))
+	(gtk-fixed-put fixed tooltip 20 20)
 	(gtk-overlay-add-overlay *help-overlay* fixed))
       (gtk-widget-show-all window)
       (setf *win* window))))
