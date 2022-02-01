33fcdc6d6813fe808be3bd61cbf1a44d27a7cd0c
hartmut <hbrng.computer@mailbox.org>
2022-02-01 21:46:56 +0100

    toolbar in separate file

--- src/load.lisp
+++ src/load.lisp
@@ -1,3 +1,4 @@
 (load "src/package.lisp")
 (load "src/f-key-defaults.lisp")
+(load "src/f-key-toolbar.lisp")
 (load "src/main.lisp")
--- src/main.lisp
+++ src/main.lisp
@@ -1,14 +1,5 @@
 (in-package :lisped)
 
-(defun create-key-button (name icon tooltip-text)
-  (let ((button (make-instance 'gtk-button :width-request 90 :tooltip-text tooltip-text))
-	 (box    (make-instance 'gtk-box :orientation :vertical :spacing 4))
-	 (label  (make-instance 'gtk-label :use-markup t :label name)))
-    (gtk-box-pack-start box (gtk-image-new-from-file icon))
-    (gtk-box-pack-start box label) 
-    (gtk-container-add button box)  
-    button))
-
 (defun create-short-help-tooltip (tooltip-text)
   (let ((css-provider (make-instance 'gtk-css-provider))
 	(box          (make-instance 'gtk-box))
@@ -22,53 +13,8 @@
 
 (defstruct key-instance button action short-help long-help)
 
-(defun create-empty-f-keys ()
-  (mapcar (lambda (key-name)
-	    (cons key-name
-		  (make-key-instance :button (create-key-button (string key-name)
-								+empty-key-icon+
-								"no action")
-				     :action nil
-				     :short-help (create-short-help-tooltip "undefined")
-				     :long-help nil)))
-	  +esc-and-f-key-names+))
-
-
-(defun create-default-keys ()
-  (mapcar (lambda (key-definition)
-	    (cons (car key-definition)
-		  (make-key-instance :button (create-key-button (string (car key-definition))
-								(key-definition-icon-file (cdr key-definition))
-								(key-definition-short-help (cdr key-definition)))
-				     :action (key-definition-action (cdr key-definition))
-				     :short-help (create-short-help-tooltip (key-definition-short-help (cdr key-definition)))
-				     :long-help nil)))
-	  +esc-and-f-keys-definitions+))
-
-(defun create-all-keys ()
-  (list (cons 'default-keys (create-default-keys))
-	(cons 'no-keys (create-empty-f-keys))))
-
 (defparameter *available-keys* nil)
 (defparameter *active-keys* nil)
-
-(defun apply-active-keys-to-toolbar (toolbar-box)
-  (dolist (key-button *active-keys*)
-    (gtk-box-pack-start toolbar-box (key-instance-button (cdr key-button)) :expand nil)))
-
-(defun activate-keys (toolbar-box new-keys)
-  (if (eql new-keys nil)
-      nil
-      (progn (print (car (car new-keys)))
-	     (activate-keys toolbar-box (cdr new-keys)))))
-
-(defun clear-toolbar (toolbar-box)
-  (let ((buttons-to-be-deleted nil))
-    (gtk-container-foreach toolbar-box
-			   (lambda (key-button)
-			     (setq buttons-to-be-deleted
-				   (append buttons-to-be-deleted (list key-button)))))
-    buttons-to-be-deleted))
 
 (defun apply-active-keys-to-help-overlay (help-overlay)
   (dolist (key-button *active-keys*)
